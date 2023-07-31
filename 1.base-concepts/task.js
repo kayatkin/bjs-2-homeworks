a3205524e811d15de245f01ab3d673f97879345c
Viktor Kayatkin <kayatkin@mail.ru>
2023-08-01 00:16:43 +0700

    first homework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,37 @@
 function solveEquation(a, b, c) {
   let arr = [];
   
-  return arr;
+  const discriminant = Math.pow(b, 2) - 4 * a * c;
+  
+  if (discriminant < 0) {
+      return arr;
+  } else if (discriminant === 0) {
+      const root = -b/(2*a);
+      return [root];
+  } else {
+      const root1 = (-b + Math.sqrt(discriminant))/(2*a);
+      const root2 = (-b - Math.sqrt(discriminant))/(2*a);
+      return [root1, root2];
+  }
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
+  if (typeof percent !== "number" || typeof contribution !== "number" ||
+     typeof amount !== "number" || typeof countMonths !== "number") {
+     return false;
+  }
+
+  const monthlyPercent = percent / 100 / 12;
+
+  const creditBody = amount - contribution;
+
+  const monthlyPayment = creditBody * (monthlyPercent + (monthlyPercent 
+      / (((1 + monthlyPercent) ** countMonths) - 1)));
+
+  const totalAmount = monthlyPayment * countMonths + contribution;
+
+  return Number(totalAmount.toFixed(2));
+
 }
