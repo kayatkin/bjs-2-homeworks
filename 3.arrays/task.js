95d2ce6b008ed83c67899ad69aee7be949a8c5f2
Viktor Kayatkin <kayatkin@mail.ru>
2023-08-20 10:10:02 +0700

    fix error

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,28 +1,18 @@
 function compareArrays(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
-        return false
-      }
-    
-      return arr1.every((element, index) => element === arr2[index]);
-    }
-    
-    module.exports = {
-      compareArrays
-}
+    return arr1.every((el, idx) => arr1.length === arr2.length && el === arr2[idx]);  
+  }
+  
+  module.exports = {
+    compareArrays
+  }
 
 function getUsersNamesInAgeRange(users, gender) {
-    const filteredUsers = users.filter(user => user.gender === gender);
-  
-    if (filteredUsers.length === 0) {
-      return 0; // Нет пользователей заданного пола, возвращаем 0
-    }
-    
-    const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
-    const averageAge = totalAge / filteredUsers.length;
-    
-    return averageAge;
+    return users.
+      filter(user => user.gender === gender).
+      map(user => user.age).
+      reduce((average, userAge, index, array) => average + userAge / array.length, 0);
   }
   
   module.exports = {
     getUsersNamesInAgeRange
-}
+  }
