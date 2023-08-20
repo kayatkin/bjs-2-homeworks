67856039ada2c99b5c73fc7b322c204cb4891c81
Viktor Kayatkin <kayatkin@mail.ru>
2023-08-20 10:04:50 +0700

    initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,28 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false
+      }
+    
+      return arr1.every((element, index) => element === arr2[index]);
+    }
+    
+    module.exports = {
+      compareArrays
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter(user => user.gender === gender);
   
+    if (filteredUsers.length === 0) {
+      return 0; // Нет пользователей заданного пола, возвращаем 0
+    }
+    
+    const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+    const averageAge = totalAge / filteredUsers.length;
+    
+    return averageAge;
+  }
+  
+  module.exports = {
+    getUsersNamesInAgeRange
 }
